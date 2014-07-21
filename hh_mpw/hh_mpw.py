b0f0ae76f237fd525f4b161c3c9cfcd824f9adea
Tyler Stephens <roberttstephens@gmail.com>
2014-07-21 16:11:57 -0400

    Calculate miles for tempo runs based on pace.

--- hh_mpw/hh_mpw.py
+++ hh_mpw/hh_mpw.py
@@ -9,17 +9,40 @@ from lxml.cssselect import CSSSelector
 import re
 import requests
 
-def get_miles(string):
+def calculate_miles(string, pace):
+    """
+    Given '30 min tempo', and 10 (minutes per miles) return the miles.
+
+    The math is set up like so
+
+    10 minutes/1 mile = 30 minutes/d miles
+    10d = 30
+    d = 3
+    """
+    pattern = re.compile(r'^(\d+\.?(\d+)?) min tempo')
+    match = pattern.match(string)
+    if match:
+        return float(match.groups()[0]) / pace
+    return 0.0
+
+def determine_miles(string, pace):
     """
     Get the number of miles in a string.
     """
-    # Matches '1 m'
+    return parse_miles(string) or calculate_miles(string, pace)
+
+def parse_miles(string):
+    """
+    Given '3 m run' return 3
+    """
     pattern = re.compile(r'^(\d+\.?(\d+)?) m[^in]')
     match = pattern.match(string)
     if match:
-        return match.groups()[0]
+        return float(match.groups()[0])
+    return 0.0
 
-def hh_mpw(url):
+
+def hh_mpw(url, pace):
     """
     Get the miles per week for a hal higdon program in a list format.
     """
@@ -33,7 +56,7 @@ def hh_mpw(url):
     for row in rows[1::]:
         columns = column_sel(row)
         # The first column is the week number.
-        week_miles = sum(list(map(float, list(filter(None, [get_miles(c.text_content()) for c in columns[1:]])))))
+        week_miles = sum(list([determine_miles(c.text_content(), pace) for c in columns[1:]]))
         mpw[int(columns[0].text_content())] = week_miles
     return mpw
 
@@ -45,9 +68,16 @@ def main():
         description='Determine the miles per week for a hal higdon program.'
     )
     parser.add_argument('url', help='The url to check')
+    parser.add_argument(
+        '-p',
+        '--pace',
+        help='Your average pace in minutes per mile.',
+        default=10.0,
+        type=float,
+    )
     args = parser.parse_args()
 
-    mpw = hh_mpw(args.url)
+    mpw = hh_mpw(args.url, args.pace)
     if mpw:
         for key, value in mpw.items():
             print(key, value, sep='\t')
